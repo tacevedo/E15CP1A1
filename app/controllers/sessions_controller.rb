3ec0f25c1f875067673b7b1577a89bc6d29703c5
tam <tamara.acevedoavila@gmail.com>
2018-06-27 23:40:16 -0400

    listo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user.nil?
+      redirect_to sign_up_path alert: 'el usuario no éxito'
+      return
+    end
+
+    if @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to sign_in_path alert: 'password no es válido'
+    end
+
+  end
+
 
   def destroy
     reset_session
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :histories
-  resources :sessions, only: [:create, :destroy]
+
   root "histories#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/sign_up', to: "users#new"
+  get 'users/sign_in', to: 'sessions#new'
   post 'users', to: "users#create"
+  resources :sessions, only: [:create, :destroy]
 end
